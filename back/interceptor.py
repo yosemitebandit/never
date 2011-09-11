b8dcafe62360a6dcf223424533bb32035825850f
mball <mball@redwoodsys.com>
2011-09-11 07:20:59 -0700

    handling incoming stuffs

--- back/interceptor.py
+++ back/interceptor.py
@@ -3,7 +3,10 @@ interceptor.py
 handling them texts
 '''
 import optparse
+import time
+import sys
 import simplejson as json
+import threading
 
 from twilio.rest import TwilioRestClient
 # load up a config file that contains an API key
@@ -19,14 +22,45 @@ def main():
     config = json.loads(f.read())
     f.close()
 
+        
+    # start a thread for each cluster's scraper;  maybe add more later
+    threadPool = {}
+    key = 'thread1'
+    threadPool[key] = threading.Thread(target=worker, args=(config,))
+    threadPool[key].start()
+    
+    # continuously check that the threads are still kicking
+    while(1):   
+        for key in threadPool:
+            if not threadPool[key].is_alive():
+                thread_print('*error, restarting %s thread' % key)
+                threadPool[key] = threading.Thread(target=worker, args=(config,))
+                threadPool[key].start()
+
+        time.sleep(10)
+
+def worker(config):
+    ''' gets threaded..scrapes the twilio sms messages
+    '''
+    thread_print('starting thread')
+    
     client = TwilioRestClient(config['twilio']['account_sid'], config['twilio']['auth_token'])
 
-    for message in client.sms.messages.list():
-        print message
-        print message.body
+    
+    while(1):
+        print dir(client.sms.messages)
+
+        for message in client.sms.messages.list():
+            if message.direction == 'inbound':
+                print message.body
+                print message.from_
         
+        time.sleep(5)
     
 
+def thread_print(message):
+    _now = time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime())
+    sys.stdout.write(_now + ' - ' + message + '\n')
 
 
 '''Parser-related
